1f830dad205f32d9e2d9d49a85c6dbfede87a500
SadRus <rystixx@yandex.ru>
2023-05-12 00:18:34 +0300

    Add '/start' command handler and inline keyboard

--- tg_bot.py
+++ tg_bot.py
@@ -2,6 +2,7 @@ import os
 import redis
 
 from dotenv import load_dotenv
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     CallbackQueryHandler,
     CommandHandler,
@@ -12,21 +13,45 @@ from telegram.ext import (
 
 
 def start(update, context):
+    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
+                 InlineKeyboardButton("Option 2", callback_data='2')],
+
+                [InlineKeyboardButton("Option 3", callback_data='3')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text='Hello, squizers!\nНажмите "Новый вопрос" для начала викторины.',
+        text='Hello, kek!',
+        reply_markup=reply_markup,
     )
+
+
+def button(update, context) -> None:
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text=f"Selected option: {query.data}")
 
 
 def main():
     load_dotenv()
 
     tg_token = os.getenv('TG_BOT_TOKEN')
+    redis_host = os.getenv('REDIS_HOST')
+    redis_port = os.getenv('REDIS_PORT')
+    redis_tg_db = os.getenv('REDIS_TG_DB')
+
+    cache = redis.Redis(
+        host=redis_host,
+        port=redis_port,
+        decode_responses=True,
+        db=redis_tg_db,
+    )
 
     updater = Updater(tg_token)
     dispatcher = updater.dispatcher
-
     dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CallbackQueryHandler(button))
 
     updater.start_polling()
     updater.idle()
