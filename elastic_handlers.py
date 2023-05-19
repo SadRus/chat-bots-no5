623c55fbb9ee133babe814db4a4f0a0798afe9de
SadRus <rystixx@yandex.ru>
2023-05-19 19:48:56 +0300

    Add a product menu and a cart menu with buttons

---
+++ elastic_handlers.py
@@ -0,0 +1,230 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+from pprint import pprint
+
+
+def get_access_token(client_id, client_secret):
+    url = 'https://api.moltin.com/oauth/access_token'
+    payload = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials',
+    }
+
+    response = requests.post(url, data=payload)
+    return response.json()['access_token']
+
+
+def get_products(token):
+    url = 'https://api.moltin.com/pcm/products'
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    products = response.json()['data']
+    return products
+
+
+def get_product_by_id(token, product_id):
+    url = f'https://api.moltin.com/pcm/products/{product_id}'
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    response = requests.get(url, headers=headers)
+    product_content = response.json()['data']
+    return product_content
+
+
+def create_cart(token, user_id):
+    url = 'https://api.moltin.com/v2/carts'
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+    }
+    payload = {
+        'data': {
+            'name': str(user_id),
+            'description': 'For a fish',
+        }
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    response.raise_for_status()
+    cart_content = response.json()['data']
+    return cart_content
+
+
+def get_cart_content(token, reference):
+    url = f'https://api.moltin.com/v2/carts/{reference}'
+    headers = {
+        'Authorization': f'Bearer {token}',
+        }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    cart_content = response.json()['data']
+    return cart_content
+
+
+def add_product_to_cart(token, cart_reference, product_id, quantity):
+    url = f'https://api.moltin.com/v2/carts/{cart_reference}/items'
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+    }
+    payload = {
+        'data': {
+            'id': product_id,
+            'type': 'cart_item',
+            'quantity': quantity,
+        }
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    response.raise_for_status()
+
+
+def get_cart_products(token, reference):
+    url = f'https://api.moltin.com/v2/carts/{reference}/items'
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    cart_products = response.json()['data']
+    return cart_products
+
+
+def remove_item_from_cart(token, cart_reference, item_id):
+    url = f'https://api.moltin.com/v2/carts/{cart_reference}/items/{item_id}'
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    response = requests.delete(url, headers=headers)
+    response.raise_for_status()
+
+
+def get_price_book(token, pricebook_id):
+    url = f'https://api.moltin.com/pcm/pricebooks/{pricebook_id}'
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    response = requests.get(url, headers=headers)
+    pricebook_content = response.json()
+    return pricebook_content
+
+
+def get_product_price(token, pricebook_id, product_price_id):
+    url = f'https://api.moltin.com/pcm/pricebooks/{pricebook_id}/prices/{product_price_id}'
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    response = requests.get(url, headers=headers)
+    produce_price = response.json()
+    return produce_price
+
+
+def get_all_prices(token, pricebook_id):
+    url = f'https://api.moltin.com/pcm/pricebooks/{pricebook_id}/prices'
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    response = requests.get(url, headers=headers)
+    prices_content = response.json()
+    return prices_content
+
+
+def get_product_image(token, product_id):
+    url = f'https://api.moltin.com/pcm/products/{product_id}/relationships/main_image'
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    response = requests.get(url, headers=headers)
+    image_relationships = response.json()['data']
+    return image_relationships
+
+
+def get_image_link(token, image_id):
+    url = f'https://api.moltin.com/v2/files/{image_id}'
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    image_content = response.json()['data']
+    image_link = image_content['link']['href']
+    return image_link
+
+    # response = requests.get(image_link)
+    # response.raise_for_status()
+    # folder = './media'
+    # filename = image_content['file_name']
+
+    # os.makedirs(folder, exist_ok=True)
+    # fullpath = os.path.join(folder, filename)
+    # with open(fullpath, 'wb') as file:
+    #     file.write(response.content)
+def get_all_carts(token):
+    url = 'https://api.moltin.com/v2/carts'
+    response = requests.get(url, headers={'Authorization': f'Bearer {token}'})
+    return response.json()
+
+
+def main():
+    load_dotenv()
+
+    client_id = os.getenv('ELASTIC_CLIENT_ID')
+    client_secret = os.getenv('ELASTIC_CLIENT_SECRET')
+    elastic_token = get_access_token(client_id, client_secret)
+
+    headers = {
+        'Authorization': elastic_token,
+    }
+
+
+    # Получить продукты
+    products = get_products(elastic_token)
+    # pprint(products[0])
+    product_id = products[0]['id']
+    product_type = products[0]['type']
+
+    # Создать корзину
+    cart_reference = create_cart(elastic_token, '440084749')['id']
+    print(f'create cart_reference: {cart_reference}')
+
+    # Получить корзину
+    cart_reference = get_cart_content(elastic_token, cart_reference)
+    print(f'get cart_reference: {cart_reference}')
+
+    # Добавить продукт в корзину
+    add_product_to_cart(
+        token=elastic_token,
+        cart_reference=cart_reference,
+        product_id=product_id,
+        quantity=2,
+    )
+
+    pprint(get_cart_products(elastic_token, cart_reference))
+    # pprint(get_all_carts(elastic_token))
+    # cart_products = get_cart_products(elastic_token, product_id)
+    # for product in products:
+    #     print(product['attributes']['name'])
+    # pprint(products[0])
+    pricebook_id = '55036149-d698-487a-8eb0-e456a543df12'
+    # pricebook = get_price_book(elastic_token, pricebook_id)
+    # pprint(pricebook)
+    # pprint(get_product_by_id(token=elastic_token, product_id='f76c5e8f-342a-4325-9b28-ed6b4a2734d2'))
+
+    # PRICEBOOK
+    # pprint(create_product_price(elastic_token, pricebook_id))
+    price_id = 'b5d09b6b-4656-473b-9d2e-6bbe35ce8667'
+    # pprint(get_all_prices(elastic_token, pricebook_id))
+    # pprint(get_product_price(elastic_token, pricebook_id=pricebook_id, product_price_id=price_id))
+
+    # IMAGE
+    # product_image_id = get_product_image(headers, product_id)['id']
+    # get_image_link(headers, product_image_id)
+
+
+if __name__ == '__main__':
+    main()
--- tg_bot.py
+++ tg_bot.py
@@ -2,6 +2,7 @@ import os
 import redis
 
 from dotenv import load_dotenv
+from functools import partial
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     CallbackQueryHandler,
@@ -10,48 +11,245 @@ from telegram.ext import (
     MessageHandler,
     Updater,
 )
+from elastic_handlers import (
+    add_product_to_cart,
+    create_cart,
+    get_access_token,
+    get_cart_content,
+    get_cart_products,
+    get_image_link,
+    get_products,
+    get_product_by_id,
+    get_product_image,
+    remove_item_from_cart,
+)
 
 
-def start(update, context):
-    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
-                 InlineKeyboardButton("Option 2", callback_data='2')],
+_database = None
 
-                [InlineKeyboardButton("Option 3", callback_data='3')]]
 
+def send_cart(update, context, elastic_token, cart_reference):
+    query = update.callback_query
+    cart_products = get_cart_products(elastic_token, cart_reference)
+    if cart_products:
+        text = ''
+        keyboard = []
+        for product in cart_products:
+            name = product['name']
+            description = product['description']
+            quantity = product['quantity']
+            unit_price = product['unit_price']['amount']
+            text += f"{name}\n"
+            text += f"{description}\n"
+            text += f"{quantity}kg in cart for ${quantity*unit_price}\n\n"
+            keyboard.append(
+                [InlineKeyboardButton(f'Удалить {name}', callback_data=f'{product["id"]}')]
+            )
+        keyboard.append(
+            [InlineKeyboardButton('В меню', callback_data='menu')]
+        )
+        cart_content = get_cart_content(elastic_token, cart_reference)
+        total_sum = cart_content['meta']['display_price']['with_tax']['amount']
+        text += f'Total: ${total_sum}'
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=text,
+            reply_markup=reply_markup,
+        )
+        context.bot.delete_message(
+            chat_id=update.effective_chat.id,
+            message_id=query.message.message_id,
+        )
+    else:
+        keyboard = [
+            [InlineKeyboardButton('В меню', callback_data='menu')],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='У вас нет товаров в корзине',
+            reply_markup=reply_markup,
+        )
+        context.bot.delete_message(
+            chat_id=update.effective_chat.id,
+            message_id=query.message.message_id,
+        )
+
+
+def get_or_create_cart(update, context, elastic_token):
+    chat_id = update.effective_chat.id
+    cart_reference = context.user_data.get('cart_reference')
+    if not cart_reference:
+        cart_reference = create_cart(elastic_token, user_id=chat_id)['id']
+        context.user_data['cart_reference'] = cart_reference
+    return cart_reference
+
+
+def start(update, context, elastic_token):
+    products = get_products(elastic_token)
+    keyboard = []
+    for product in products:
+        keyboard.append(
+            [InlineKeyboardButton(f'{product["attributes"]["name"]}', callback_data=product['id'])]
+        )
+    keyboard.append([InlineKeyboardButton('Корзина', callback_data='cart')])
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text='Hello, kek!',
+        text='Please choose:',
         reply_markup=reply_markup,
     )
+    return 'HANDLE_MENU'
 
 
-def button(update, context) -> None:
+def button(update, context, elastic_token):
     query = update.callback_query
     query.answer()
-    query.edit_message_text(text=f"Selected option: {query.data}")
+
+    if query.data == 'cart':
+        cart_reference = context.user_data.get('cart_reference')
+        send_cart(update, context, elastic_token, cart_reference)
+        return 'HANDLE_CART'
+
+    keyboard = [
+        [
+            InlineKeyboardButton('1 kg', callback_data=1),
+            InlineKeyboardButton('5 kg', callback_data=5),
+            InlineKeyboardButton('10 kg', callback_data=10)
+        ],
+        [InlineKeyboardButton('Назад', callback_data='back')],
+        [InlineKeyboardButton('Корзина', callback_data='cart')],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    product_id = query.data
+    context.user_data['product_id'] = product_id
+    product = get_product_by_id(elastic_token, product_id)['attributes']
+    image_id = get_product_image(elastic_token, product_id)['id']
+    image_link = get_image_link(elastic_token, image_id)
+
+    context.bot.send_photo(
+        chat_id=update.effective_chat.id,
+        caption=f"{product['name']}\n\n"
+                f"{product['description']}\n"
+                f"{product['slug']}\n",
+        photo=f'{image_link}',
+        reply_markup=reply_markup,
+    )
+    context.bot.delete_message(
+        chat_id=update.effective_chat.id,
+        message_id=query.message.message_id,
+    )
+    return 'HANDLE_DESCRIPTION'
+
+
+def description_button(update, context, elastic_token):
+    query = update.callback_query
+    query.answer()
+    if query.data == 'back':
+        start(update, context, elastic_token)
+        context.bot.delete_message(
+            chat_id=update.effective_chat.id,
+            message_id=query.message.message_id,
+        )
+        return 'HANDLE_MENU'
+    if query.data == 'cart':
+        cart_reference = context.user_data.get('cart_reference')
+        send_cart(update, context, elastic_token, cart_reference)
+        return 'HANDLE_CART'
+    else:
+        cart_reference = get_or_create_cart(update, context, elastic_token)
+        product_id = context.user_data['product_id']
+        quantity = int(query.data)
+        add_product_to_cart(
+            token=elastic_token,
+            cart_reference=cart_reference,
+            product_id=product_id,
+            quantity=quantity,
+        )
+        return 'HANDLE_DESCRIPTION'
+
+
+def cart_button(update, context, elastic_token):
+    query = update.callback_query
+    query.answer()
+    if query.data == 'menu':
+        start(update, context, elastic_token)
+        context.bot.delete_message(
+            chat_id=update.effective_chat.id,
+            message_id=query.message.message_id,
+        )
+        return 'HANDLE_MENU'
+    else:
+        cart_reference = context.user_data['cart_reference']
+        remove_item_from_cart(elastic_token, cart_reference, query.data)
+        send_cart(update, context, elastic_token, cart_reference)
+        return 'HANDLE_CART'
+
+
+def handle_users_reply(update, context):
+    db = get_database_connection()
+    client_id = os.getenv('ELASTIC_CLIENT_ID')
+    client_secret = os.getenv('ELASTIC_CLIENT_SECRET')
+    elastic_token = get_access_token(client_id, client_secret)
+
+    if update.message:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query:
+        user_reply = update.callback_query.data
+        chat_id = update.callback_query.message.chat_id
+    else:
+        return None
+
+    if user_reply == '/start':
+        user_state = 'START'
+    else:
+        user_state = db.get(chat_id)
+
+    states_functions = {
+        'START': start,
+        'HANDLE_MENU': button,
+        'HANDLE_DESCRIPTION': description_button,
+        'HANDLE_CART': cart_button,
+    }
+    state_handler = states_functions[user_state]
+    try:
+        next_state = state_handler(
+            update,
+            context,
+            elastic_token=elastic_token,
+        )
+        db.set(chat_id, next_state)
+    except Exception as err:
+        print(err)
+
+
+def get_database_connection():
+    global _database
+    if _database is None:
+        database_host = os.getenv("REDIS_HOST")
+        database_port = os.getenv("REDIS_PORT")
+        _database = redis.Redis(
+            host=database_host,
+            port=database_port,
+            decode_responses=True,
+        )
+    return _database
 
 
 def main():
     load_dotenv()
 
     tg_token = os.getenv('TG_BOT_TOKEN')
-    redis_host = os.getenv('REDIS_HOST')
-    redis_port = os.getenv('REDIS_PORT')
-    redis_tg_db = os.getenv('REDIS_TG_DB')
-
-    cache = redis.Redis(
-        host=redis_host,
-        port=redis_port,
-        decode_responses=True,
-        db=redis_tg_db,
-    )
 
     updater = Updater(tg_token)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CallbackQueryHandler(button))
+    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
+    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
 
     updater.start_polling()
     updater.idle()
